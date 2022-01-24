363f8975867115e1c229ab2083dd2c463ae2a711
Fatih <rosten1998@gmail.com>
2022-01-24 19:21:29 +0300

    Виден  список кликабельный  ,добавлено долгое нажатие на элементы  списка ,кнопка добавления без функций .

--- app/src/main/java/com/example/alko/domain/GetShopAlkoList.kt
+++ app/src/main/java/com/example/alko/domain/GetShopAlkoList.kt
@@ -1,8 +1,10 @@
 package com.example.alko.domain
 
+import androidx.lifecycle.LiveData
+
 class GetShopAlkoList(private val shopListRepository:ShopListRepository) {
 
-    fun getShopList(): List<ShopItem>{
+    fun getShopList(): LiveData<List<ShopItem>> {
         return shopListRepository.getShopList()
 
     }
